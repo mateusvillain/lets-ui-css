5b17d543b213c9e492677b5bc64e5ed4a91b1fc7
Mateus Villain <mateusvillain@gmail.com>
2025-05-02 13:40:23 -0300

    feat: add multibrand tokens and new typography tokens

--- build-output.js
+++ build-output.js
@@ -4,13 +4,13 @@ import StyleDictionary from 'style-dictionary';
 register(StyleDictionary);
 
 const sd = new StyleDictionary({
-  source: ['tokens/**/*.json'],
+  source: ['src/tokens/**/*.json'],
   preprocessors: ['tokens-studio'],
   platforms: {
     css: {
       transformGroup: 'tokens-studio',
       transforms: ['name/kebab'],
-      buildPath: 'dist/',
+      buildPath: 'src/tokens/',
       files: [
         {
           destination: 'variables.css',
@@ -21,7 +21,7 @@ const sd = new StyleDictionary({
     scss: {
       transformGroup: 'tokens-studio',
       transforms: ['name/kebab'],
-      buildPath: 'dist/',
+      buildPath: 'src/tokens/',
       files: [
         {
           destination: 'variables.scss',
