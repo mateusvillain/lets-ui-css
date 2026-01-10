cd6b75cc5ae21733465d36a329d67128df0efe48
Mateus Villain <mateusvillain@gmail.com>
2026-01-10 11:20:33 -0300

    feat(storybook): add theme switcher functionality

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,18 +3,40 @@ const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
     a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: "todo"
-    }
+      test: 'todo',
+    },
   },
+
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Tema visual',
+      defaultValue: 'light',
+      toolbar: {
+        icon: 'circlehollow',
+        items: [
+          { value: 'light', title: 'Light' },
+          { value: 'dark', title: 'Dark' },
+        ],
+      },
+    },
+  },
+
+  decorators: [
+    (Story, context) => {
+      const { theme } = context.globals;
+
+      document.documentElement.setAttribute('data-theme', theme);
+
+      return Story();
+    },
+  ],
 };
 
 export default preview;
