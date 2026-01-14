9882232512211815851e412ee60b5078d8763651
Mateus Villain <mateusvillain@gmail.com>
2026-01-14 19:34:02 -0300

    docs(textfield): add Text Field documentation to Storybook

---
+++ docs/components/TextField/TextField.stories.js
@@ -0,0 +1,40 @@
+import '../../../dist/letsui.tokens.css';
+import '../../../dist/letsui.css';
+
+export default {
+  title: 'Components/Text Field',
+  argTypes: {
+    label: {
+      control: 'text',
+    },
+    placeholder: {
+      control: 'text',
+    },
+  },
+};
+
+const Template = ({ label, placeholder }) => {
+  return `
+    <div class="form-group">
+      <label>${label}</label>
+      <input type="text" class="form-size-lg" placeholder="${placeholder}">
+    </div>
+  `;
+};
+
+export const TextField = Template.bind({});
+TextField.args = {
+  label: 'Name',
+  placeholder: 'Type here',
+};
+
+export const Primary = () => `
+  <div class="alert bc-caution>
+    <div class="alert-content">
+      <div class="alert-text">
+        <p class="body-lg">Title</p>
+        <p class="body-lg">Title</p>
+      </div>
+    </div>
+  </div>
+`;
