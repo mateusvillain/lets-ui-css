a67845f13ccc371878ea395f94499ff76c82736d
Mateus Villain <mateusvillain@gmail.com>
2026-05-03 00:20:14 -0300

    refactor(playground): update playground

--- packages/playground/astro.config.mjs
+++ packages/playground/astro.config.mjs
@@ -1,3 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import path from 'path';
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  }
+});
